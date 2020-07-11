429cfa1cfbff4f8a2f55b85d740e6dcf78e0b8ec
James Gale <jgaleiphone@gmail.com>
2020-07-11 14:45:38 -0400

    reviews need help

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,33 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import ReviewModal from "./header/ReviewModal";
 
+//WE NEED HEEEEELLLLLPPPPPPPP
 function SingleCard(props) {
-  console.log("props", props)
+  const [showReviewModal, setShowReviewModal] = useState(false);
+  console.log("singlecard", props);
   // const test = props.review.map()
   // console.log("test", test)
+
   return (
-    <div id="container">
-      <div class="product-details">
-        <h1>{props.itemname}</h1>
+    <>
+      {showReviewModal && (
+        <ReviewModal setShowReviewModal={props.review[0][0].review} />
+      )}
+      <div id="container">
+        <div class="product-details">
+          <h1>{props.itemname}</h1>
+          <button
+            id="review"
+            onClick={() => {
+              setShowReviewModal(!showReviewModal);
+            }}
+          >
+            Reviews
+          </button>
+        </div>
 
-        {/*  <span class="hint-star star">
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star" aria-hidden="true"></i>
-          <i class="fa fa-star-half-o" aria-hidden="true"></i>
-          <i class="fa fa-star-o" aria-hidden="true"></i>
-  </span>*/}
-      <button id="review" onClick={console.log()}>Reviews</button>
-      {props.review.map(review => {return <span>{review.review}</span>})}
-      </div>
+        <div class="product-image">
+          <img src={props.image} alt="Omar Dsoky" />
 
-      <div class="product-image">
-        <img src={props.image} alt="Omar Dsoky" />
-
-        <div class="info">
-          <p>{props.description}</p>
+          <div class="info">
+            <p>{props.description}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
